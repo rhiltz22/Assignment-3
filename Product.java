50a79d49b694bc7274dceeac9d782c84d193764a
rhiltz22 <167123529+rhiltz22@users.noreply.github.com>
2024-11-17 17:18:58 -0600

    Update Product.java

--- Product.java
+++ Product.java
@@ -2,16 +2,16 @@ public class Product {
     String id;
     String name;
     String category;
-    double price;
+    String price;
 
-    public Product(String id, String name, String category, double price){
+    public Product(String id, String name, String category, String price){
         this.id = id;
         this.name = name;
         this.category = category;
         this.price = price;
     }
 
-    public double getPrice() {
+    public String getPrice() {
         return price;
     }
 
